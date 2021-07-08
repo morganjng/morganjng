843659125b3f7155364045caedad4300184d0f5a
morganjng <morganjng@gmail.com>
2021-07-07 20:20:03 -0400

    H

--- scriptHome.js
+++ scriptHome.js
@@ -1,12 +1,12 @@
 window.onscroll = function () { scroll() };
 
-var top = document.getElementById("top");
-var height = top.offsetTop;
+var scrollheader = document.getElementById("scrollheader");
+var height = scrollheader.offsetTop;
 
 function scroll() {
     if(window.pageYOffset > height) {
-        top.classList.add("scroll");
+        scrollheader.classList.add("scroll");
     } else {
-        top.classList.remove("scroll");
+        scrollheader.classList.remove("scroll");
     }
 }
